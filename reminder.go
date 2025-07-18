f57a898c1825c896cda33f35061a08e064b80859
kresswell <help@infinityhackers.tech>
2025-07-18 15:03:10 +0300

    v1

--- reminder.go
+++ reminder.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
 
 func main() {
-	fmt.Println("Hello")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New("8021513242:AAGBbCAFT_YR2TaR60NkezoTeJgJedIry40", opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+	if err != nil {
+		return
+	}
 }
